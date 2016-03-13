9bb86e64ee5f127e58916402d3e40111783d8e39
briangilbreath <bgilbreath87@gmail.com>
2016-03-12 19:56:32 -0500

    added styling

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,5 @@ var ReactDOM = require('react-dom');
 var ListManager = require('./components/ListManager.jsx');
 
 ReactDOM.render(<ListManager title="Ingredients" />, document.getElementById('ingredients'));
-
-
 ReactDOM.render(<ListManager title="TODO" />, document.getElementById('todo'));
+ReactDOM.render(<ListManager title="Christmas" headingColor="#b31217"/>, document.getElementById('christmas'));
